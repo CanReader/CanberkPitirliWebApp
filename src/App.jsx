53e7afc9712c00df3ab1c07d1162aaa967fd839e
Canberk <canberk_nurcan@hotmail.com>
2024-06-26 22:11:04 +0300

    The project has been initialized

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import {useState, useEffect} from 'react';
+
+
+function App() {
+  return (
+    <>
+      <h1>Canberk Pitirli's Universe!</h1>
+      <div className='CenterDiv'>
+        <p>
+          This page is under construction and it will be available to everybody soon<br/>
+          Please don't ask WHEN!
+        </p>
+        <p>
+        </p>
+        <img src='https://media0.giphy.com/media/Mah9dFWo1WZX0WM62Q/200w.gif?cid=6c09b952h27y4qbi6ox5daqzosyk46txk6a22t7edntloamv&ep=v1_gifs_search&rid=200w.gif&ct=g'/>
+      </div>
+    </>
+  )
+}
+
+export default App;
