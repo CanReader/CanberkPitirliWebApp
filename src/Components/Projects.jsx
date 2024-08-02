506eeef0b528cbe9336ba6fcd56324abc3ef8bfb
Canberk <canberk_nurcan@hotmail.com>
2024-08-02 13:58:44 +0300

    Added new projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,20 +6,58 @@ import { Button } from "@mui/material";
 import { Carousel } from "react-responsive-carousel";
 import { Progress } from 'react-sweet-progress';
 
+//Focus Kingdom images
 import FKLogo from '/images/FKLogo.png';
 import FKAd1 from '/images/FK_Ad1.png';
 import FKAd2 from '/images/FK_Ad2.png';
 import FKAd3 from '/images/FK_Ad3.png';
 import FKAd4 from '/images/FK_Ad4.png';
 
+//DXCraft images
+import DXCraftLogo from '../../public/images/DXCraft Logo.png';
+import DXCraft1 from '/images/DXCraft1.png';
+import DXCraft2 from '/images/DXCraft2.png';
+import DXCraft3 from '/images/DXCraft3.png';
+import DXCraft4 from '/images/DXCraft4.png';
+import DXCraft5 from '/images/DXCraft5.png';
+
+//Brokeout images
+import Brokeout1 from '/images/Brokeout1.png';
+import Brokeout2 from '/images/Brokeout2.png';
+import Brokeout3 from '/images/Brokeout3.png';
+import Brokeout4 from '/images/Brokeout4.png';
+import Brokeout5 from '/images/Brokeout5.png';
+import Brokeout6 from '/images/Brokeout6.png';
+
+import Rubycafe1 from '/images/Rubycafe1.png';
+import Rubycafe2 from '/images/Rubycafe2.png';
+
+//Programming languages
+import CPPLogo from '../../public/images/Cpp Logo.svg';
+import CSLogo from '../../public/images/Cs Logo.svg';
 import JLogo from '../../public/images/Java Logo.svg';
 import JSLogo from '../../public/images/JS Logo.svg';
-import LibGDXLogo from '../../public/images/LibGDX Logo.svg';
+
+//Operating systems
+import WindowsLogo from '../../public/images/Windows Logo.svg';
 import AndroidLogo from '../../public/images/Android Logo.svg';
 import AppleLogo from '../../public/images/Apple Logo.svg';
-import ASLogo from '../../public/images/AS Logo.svg';
-import MYLogo from '../../public/images/MySQL Logo.svg';
+
+//Technologies
+import GLLogo from '../../public/images/GL Logo.svg';
+import DXLogo from '../../public/images/DirectX Logo.svg';
+import UELogo from '../../public/images/UE Logo.svg';
+import UELogo2 from '../../public/images/UE Logo2.svg';
+import ULogo from '../../public/images/Unity Logo.svg';
+import LibGDXLogo from '../../public/images/LibGDX Logo.svg';
 import NodeLogo from '../../public/images/Node Logo.svg';
+import WPFLogo from '../../public/images/WPF Logo.svg';
+import ASLogo from '../../public/images/AS Logo.svg';
+import VSLogo from '../../public/images/VS Logo.svg';
+
+//Databses
+import MYLogo from '../../public/images/MySQL Logo.svg';
+import PSQL from '../../public/images/PostgreSQL Logo.svg';
 
 import 'react-vertical-timeline-component/style.min.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -38,14 +76,93 @@ const projects = [
       type: 'App',
       language: [JLogo, JSLogo],
       iconBackground: `linear-gradient(to right,${'#F2B40C'},90%,${'#EDCE24'})`,
-      progress: 1,
+      progress: 0.8,
       progressStatus: 'Completed',
       platform:[AndroidLogo, AppleLogo],
       techs:[ASLogo,LibGDXLogo,MYLogo,NodeLogo],
       git:'https://github.com/CanReader/FocusKingdomServer',
-      store:'',
+      store:'https://play.google.com/store/apps/details?id=com.focus.kingdom.release',
       images: [FKAd1,FKAd2,FKAd3,FKAd4],
     },
+    {
+        id: 1,
+        title: 'DXCraft',
+        logo: DXCraftLogo,
+        desc: 'This is a small-scale Minecraft-inspired project developed as a learning exercise using C++ and DirectX. Its primary purpose is to explore 3D graphics programming concepts, world generation, and game engine fundamentals.\
+         While it features basic block-based gameplay, it lacks the depth and complexity of a full-fledged game.',
+        date: '2022',
+        isVisible:true,
+        type: 'Game',
+        language: [CPPLogo],
+        iconBackground: `#4881bf`,
+        progress: 0.1,
+        progressStatus: 'Completed',
+        platform:[WindowsLogo],
+        techs:[DXLogo,VSLogo,'https://www.cdnlogo.com/logos/m/40/microsoft-xna.svg'],
+        git:'https://github.com/CanReader/DXCraft',
+        images: [DXCraft1,DXCraft2,DXCraft3,DXCraft4,DXCraft5],
+      },
+      {
+        id: 2,
+        title: 'RubySoft',
+        desc: 'RubySoft is firstly started to fix problems of most POS applications for restaurant like organizations.\
+        ',
+        date: '2021',
+        isVisible:true,
+        type: 'Application',
+        language: [CSLogo],
+        iconBackground: `#8969af`,
+        progress: 0.95,
+        progressStatus: 'Completed',
+        platform:[WindowsLogo],
+        techs:[WPFLogo,VSLogo,PSQL],
+        git:'https://github.com/CanReader/Ruby',
+        images: [
+            Rubycafe1,
+            Rubycafe2
+        ],
+      },
+      ,
+      {
+        id: 3,
+        title: 'Brokeout',
+        desc: 'A 3D breakout game made with OpenGL and GLFW. This project is initialized for learning the path of OpenGL,\
+        inspired by Minecraft',
+        date: '2020',
+        isVisible:true,
+        type: 'Game',
+        language: [CPPLogo],
+        iconBackground: `#4881bf`,
+        progress: 1,
+        progressStatus: 'Completed',
+        platform:[WindowsLogo],
+        techs:[GLLogo, VSLogo],
+        git:'https://github.com/CanReader/Brokeout',
+        images: [Brokeout1,Brokeout2,Brokeout3,Brokeout4,Brokeout5,Brokeout6],
+      },
+      {
+        id: 4,
+        title: 'Endless Combat',
+        desc: 'Endless Combat, is a coop game which is about fighting as characters with unique skills against zombies,\
+    the game also contains free for all mode; Unreal Engine is used as game engine to create a better network system,effects,game mechanics and maps.\
+    After 2023, the game has been updated as free to play.',
+        date: '2014-2016',
+        isVisible:true,
+        type: 'Game',
+        language: [CPPLogo],
+        iconBackground: `#4881bf`,
+        progress: 1,
+        progressStatus: 'Completed',
+        platform:[WindowsLogo],
+        techs:[UELogo,PSQL],
+        store:'https://store.steampowered.com/app/690350/Endless_Combat/',
+        images: [
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/690350/ss_45f4c133cff300ca2d16e390727b781b15404fd7.600x338.jpg?t=1624199444',
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/690350/ss_d85a06ac754e699fb2b361cb519a30e9b235645a.1920x1080.jpg?t=1624199444',
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/690350/ss_d7592531721a18d17f1e2df8f6847ed74c19809e.600x338.jpg?t=1624199444',
+            'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/690350/ss_852f48b4a2bf12133a4877a7d15e0d5d423256f1.600x338.jpg?t=1624199444'
+        ],
+      },
   ];
 
 const ProjectSection = () => {
@@ -59,7 +176,7 @@ const ProjectSection = () => {
 
     const getAppLogo = (project) => {
         if(project.logo)
-            return <img src={project.logo} width='40'/>
+            return <img src={project.logo} style={{height:'35px',marginRight:5}}/>
     }
 
     const getPlatforms = (platforms) => {
@@ -86,8 +203,8 @@ const ProjectSection = () => {
                 imageList.push(<img
                     src={image} 
                     style={{
+                        height:400,
                         objectFit:'contain',
-                        width:'50%'
                     }}
                     alt='Image'/>);
         return imageList;
@@ -95,8 +212,16 @@ const ProjectSection = () => {
 
     const repoButton = (link) => {
     return link ? 
-        <Button variant="contained" style={{marginTop:'2vh'}} href="https://github.com/CanReader/FocusKingdomServer/">
+        <Button variant="contained" style={{marginTop:'2vh',marginRight:5,fontSize:5}} href={link}>
             To git repository
+            </Button> 
+        : <></>
+    };
+
+    const storeButton = (link) => {
+    return link ? 
+        <Button variant="contained" style={{marginTop:'2vh',marginRight:5,fontSize:5}} href={link}>
+            Get Store
             </Button> 
         : <></>
     };
@@ -167,6 +292,7 @@ const ProjectSection = () => {
                         </Carousel>
 
                         {repoButton(project.git)}
+                        {storeButton(project.store)}
                 </div>
               </VerticalTimelineElement>
 
